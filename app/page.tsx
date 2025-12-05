ad12c997fc86a4c831d5bd7aa2df9c6a397a58e7
miamiamia <42030043+linhaishe@users.noreply.github.com>
2025-12-05 14:07:02 +0800

    feat: add icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ExploreBtn from "@/components/exploreBtn";
 
 const Home = () => {
   return (
     <div>
       <h1 className="text-center">The hub for Every Dev < br /> Event You Can&apos;t Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn />
     </div>
   );
 };
